31edd0c4a6d513b0a9ce52b46001fcedee9242d2
Emmanuel Lafenwa <leoemaxie@gmail.com>
2024-02-07 00:32:20 +0100

    Database Setup

---
+++ backend/src/config/db.config.ts
@@ -0,0 +1,26 @@
+import { DocumentStore } from "ravendb";
+import * as fs from "fs";
+
+export async function connectDB(
+    certPath: string,
+    dbUrl: string,
+    dbName: string = "Omni"
+): Promise<DocumentStore> {
+    const store = new DocumentStore(dbUrl, dbName);
+    const cert = fs.readFileSync(certPath);
+
+    store.authOptions = {
+        certificate: cert,
+        type: "pfx",
+    };
+    store.initialize();
+
+    try {
+        await store.openSession().load("documents/1-A");
+        console.log("Connected to Database");
+        return store;
+    } catch (error) {
+        console.error("Error connecting to Database:", error);
+        throw error;
+    }
+}
